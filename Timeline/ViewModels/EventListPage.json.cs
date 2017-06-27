6314275c62b27dcf899652328debcfa3a0af6cd3
Dimitris Thanasis <Dimitris.th@hotmail.com>
2017-06-27 13:02:09 +0200

    Work In Progress

--- Timeline/ViewModels/EventListPage.json.cs
+++ Timeline/ViewModels/EventListPage.json.cs
@@ -16,41 +16,73 @@ namespace Timeline
         static EventListPage()
         {
             DefaultTemplate.Events.Bind = nameof(bindEvents);
-            DefaultTemplate.EventGroups.Bind = nameof(bindEventGroups);
+            //DefaultTemplate.EventGroups.Bind = nameof(bindEventGroups);
         }
 
-        public List<EventGroupsElementJson> bindEventGroups
+        protected override void OnData()
         {
-            get
-            {
-                Person thisPerson = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.PersonId)) as Person;
-                List<EventGroupsElementJson> testList = new List<EventGroupsElementJson>();
-                var test = Db.SQL<Event>("SELECT e FROM Simplified.Ring1.Event e").OrderByDescending(x => x.EventInfo.Created).ToList().GroupBy(x => x.EventInfo.Created.ToString("dd MMMM, yyyy")).Select(x => x.ToList()).ToList();
-                if (!string.IsNullOrEmpty(this.PersonId))
-                {
-                    test = test.Select(x => x.Where(y => y.Participants.Contains(thisPerson) || y.Participants.First == null).ToList()).ToList();
-                }
-                if (!string.IsNullOrEmpty(HelperFunctions.CurrentSortSelection))
-                {
-                    test = test.Select(x => x.Where(y => y.Name == HelperFunctions.CurrentSortSelection).ToList()).ToList();
-                }
-                foreach (var item in test)
-                {
-                    if (item.Count == 0)
-                    {
-                        break;
-                    }
-                    EventGroupsElementJson newGroup = new EventGroupsElementJson();
-                    newGroup.Date.Day = item[0].EventInfo.Created.Day.ToString();
-                    newGroup.Date.Month = item[0].EventInfo.Created.Month.ToString("MMM");
-                    newGroup.Date.Year = item[0].EventInfo.Created.Year.ToString();
-                    newGroup.Events.Data = item;
-                    testList.Add(newGroup);
-                }
+            base.OnData();
 
-                return testList;
-            }
+            //List<EventListEventGroups> testList = new List<EventListEventGroups>();
+            //var test = Db.SQL<Event>("SELECT e FROM Simplified.Ring1.Event e").OrderByDescending(x => x.EventInfo.Created).ToList().GroupBy(x => x.EventInfo.Created.ToString("dd MMMM, yyyy")).Select(x => x.ToList()).ToList();
+            //if (!string.IsNullOrEmpty(this.PersonId))
+            //{
+            //    Person thisPerson = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.PersonId)) as Person;
+            //    test = test.Select(x => x.Where(y => y.Participants.Contains(thisPerson) || y.Participants.First == null).ToList()).ToList();
+            //}
+            //if (!string.IsNullOrEmpty(HelperFunctions.CurrentSortSelection))
+            //{
+            //    test = test.Select(x => x.Where(y => y.Name == HelperFunctions.CurrentSortSelection).ToList()).ToList();
+            //}
+            //foreach (var item in test)
+            //{
+            //    if (item.Count == 0)
+            //    {
+            //        break;
+            //    }
+            //    EventListEventGroups newGroup = new EventListEventGroups();
+            //    newGroup.Date.Day = item[0].EventInfo.Created.Day.ToString();
+            //    newGroup.Date.Month = item[0].EventInfo.Created.Month.ToString("MMM");
+            //    newGroup.Date.Year = item[0].EventInfo.Created.Year.ToString();
+            //    newGroup.Events.Data = item;
+            //    testList.Add(newGroup);
+            //}
+
+            //this.EventGroups.Data = testList;
         }
+
+        //public List<EventListEventGroups> bindEventGroups
+        //{
+        //    get
+        //    {
+        //        List<EventListEventGroups> testList = new List<EventListEventGroups>();
+        //        var test = Db.SQL<Event>("SELECT e FROM Simplified.Ring1.Event e").OrderByDescending(x => x.EventInfo.Created).ToList().GroupBy(x => x.EventInfo.Created.ToString("dd MMMM, yyyy")).Select(x => x.ToList()).ToList();
+        //        if (!string.IsNullOrEmpty(this.PersonId))
+        //        {
+        //            Person thisPerson = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.PersonId)) as Person;
+        //            test = test.Select(x => x.Where(y => y.Participants.Contains(thisPerson) || y.Participants.First == null).ToList()).ToList();
+        //        }
+        //        if (!string.IsNullOrEmpty(HelperFunctions.CurrentSortSelection))
+        //        {
+        //            test = test.Select(x => x.Where(y => y.Name == HelperFunctions.CurrentSortSelection).ToList()).ToList();
+        //        }
+        //        foreach (var item in test)
+        //        {
+        //            if (item.Count == 0)
+        //            {
+        //                break;
+        //            }
+        //            EventListEventGroups newGroup = new EventListEventGroups();
+        //            newGroup.Date.Day = item[0].EventInfo.Created.Day.ToString();
+        //            newGroup.Date.Month = item[0].EventInfo.Created.Month.ToString("MMM");
+        //            newGroup.Date.Year = item[0].EventInfo.Created.Year.ToString();
+        //            newGroup.Events.Data = item;
+        //            testList.Add(newGroup);
+        //        }
+
+        //        return testList;
+        //    }
+        //}
 
         public List<Event> bindEvents
         {
@@ -86,9 +118,6 @@ namespace Timeline
             //    returnEvents.AddRange(newEvents);
             //    return returnEvents.OrderByDescending(x => x.EventInfo.Created).ToList();
             //}
-
-
-
             get
             {
                 List<Event> allEvents = Db.SQL<Event>("SELECT e FROM Simplified.Ring1.Event e").ToList();
@@ -110,14 +139,30 @@ namespace Timeline
         }
     }
 
-    [EventListPage_json.EventGroups.Events]
+    [EventListPage_json.EventGroups]
+    partial class EventListEventGroups
+    {
+        public DateTimeFormatInfo DateInfo = new DateTimeFormatInfo();
+
+        //public EventListPage ParentPage
+        //{
+        //    get
+        //    {
+        //        return this.Parent.Parent as EventListPage;
+        //    }
+        //}
+
+        //public string PersonId { get { return ""; } }
+    }
+
+    [EventListPage_json.Events]
     partial class EventListPageEvents
     {
         public EventListPage ParentPage
         {
             get
             {
-                return this.Parent.Parent.Parent.Parent as EventListPage;
+                return this.Parent.Parent as EventListPage;
             }
         }
 
@@ -125,6 +170,26 @@ namespace Timeline
         {
             DefaultTemplate.DisplayedDate.Bind = nameof(bindDate);
             DefaultTemplate.Participant.Bind = nameof(bindParticipant);
+            DefaultTemplate.DisplayBreak.Bind = nameof(bindDisplayBreak);
+        }
+
+        public bool bindDisplayBreak
+        {
+            get
+            {
+                Event thisEvent = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.Key)) as Event;
+                List<Event> allEvents = Db.SQL<Event>("SELECT e FROM Simplified.Ring1.Event e").OrderByDescending(x => x.EventInfo.Created).ToList();
+                var thisIndex = allEvents.IndexOf(thisEvent);
+                if (allEvents[0].GetObjectID() == this.Data.GetObjectID())
+                {
+                    return true;
+                }
+                if (allEvents[thisIndex].EventInfo.Created.ToString("dd MMM, yyyy") != allEvents[thisIndex - 1].EventInfo.Created.ToString("dd MMM, yyyy"))
+                {
+                    return true;
+                }
+                return false;
+            }
         }
 
         public string bindDate
@@ -190,6 +255,97 @@ namespace Timeline
             });
         }
         
+        public void Handle(Input.DeleteTrigger Action)
+        {
+            Event thisEvent = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.Key)) as Event;
+            HelperFunctions.DeleteEvent(thisEvent);
+        }
+    }
+
+    [EventListPage_json.EventGroups.Events]
+    partial class EventListEventGroupsEvent
+    {
+        public EventListPage ParentPage
+        {
+            get
+            {
+                return this.Parent.Parent.Parent.Parent as EventListPage;
+            }
+        }
+
+        static EventListEventGroupsEvent()
+        {
+            //DefaultTemplate.DisplayedDate.Bind = nameof(bindDate);
+            DefaultTemplate.Participant.Bind = nameof(bindParticipant);
+        }
+
+        public string bindDate
+        {
+            get
+            {
+                ParentPage.DateInfo.LongDatePattern = "HH:mm dddd dd MMMM";
+                DateTime currentDate = (DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.Key)) as Event).EventInfo.Created;
+                this.Date.Day = currentDate.Day.ToString();
+                this.Date.Month = currentDate.ToString("MMM");
+                this.Date.Year = currentDate.Year.ToString();
+                return currentDate.ToString(ParentPage.DateInfo.LongDatePattern);
+            }
+        }
+
+        public string bindParticipant
+        {
+            get
+            {
+                Event thisEvent = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.Key)) as Event;
+                //What if there are multiple participants?
+                Person participant = thisEvent?.Participants?.First as Person;
+                return participant?.Name;
+            }
+        }
+
+        protected override void OnData()
+        {
+            base.OnData();
+            if (string.IsNullOrEmpty(this.Key))
+            {
+                return;
+            }
+            this.TimelineEventPage = Self.GET($"/timeline/timeline-item/{this.Key}");
+
+            Event thisEvent = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.Key)) as Event;
+            List<Event> allEvents = Db.SQL<Event>("SELECT ep.Event FROM Simplified.Ring6.EventParticipation ep").ToList();
+
+            //If an event does not have a relation > attach it to "this user".
+            //The only time this will happen is when an event has just been created. = The event will be attached to the correct person
+            if (!allEvents.Contains(thisEvent) && !string.IsNullOrEmpty(this.ParentPage.PersonId))
+            {
+                Person thisPerson = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.ParentPage.PersonId)) as Person;
+                Db.Transact(() =>
+                {
+                    EventParticipation eventParticipation = new EventParticipation();
+                    eventParticipation.Event = thisEvent;
+                    eventParticipation.Participant = thisPerson;
+                });
+            }
+            else if (!allEvents.Contains(thisEvent) && string.IsNullOrEmpty(this.ParentPage.PersonId)) // If an event is created outside of a person scope, it should displayed for every user
+            {
+                Db.Transact(() =>
+                {
+                    EventParticipation eventParticipation = new EventParticipation();
+                    eventParticipation.Event = thisEvent;
+                });
+            }
+        }
+
+        public void Handle(Input.EditTrigger Action)
+        {
+            Event thisEvent = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.Key)) as Event;
+            Db.Transact(() =>
+            {
+                thisEvent.EventInfo.Updated = DateTime.Now;
+            });
+        }
+
         public void Handle(Input.DeleteTrigger Action)
         {
             Event thisEvent = DbHelper.FromID(DbHelper.Base64DecodeObjectID(this.Key)) as Event;
